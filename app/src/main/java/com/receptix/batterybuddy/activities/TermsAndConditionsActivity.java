e4b5bf6251f18e14e8a4e0d1fc1c43241a202cb5
DESKTOP-7MTN7HU\hello <tenzin@zero1.io>
2017-05-24 17:34:12 +0530

    about us upda

--- app/src/main/java/com/receptix/batterybuddy/activities/TermsAndConditionsActivity.java
+++ app/src/main/java/com/receptix/batterybuddy/activities/TermsAndConditionsActivity.java
@@ -57,7 +57,7 @@ import static com.receptix.batterybuddy.helper.Constants.Urls.URL_OZOCK;
 
 public class TermsAndConditionsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView textViewTermsOfService, textViewPrivacyPolicy;
+    TextView textViewPrivacyPolicy;
     UserSessionManager userSessionManager;
     Context context;
     Button agreeBtn;
@@ -236,21 +236,17 @@ public class TermsAndConditionsActivity extends AppCompatActivity implements Vie
 
     private void initView() {
 
-        textViewTermsOfService = (TextView) findViewById(R.id.textview_terms_of_service);
         textViewPrivacyPolicy = (TextView) findViewById(R.id.textview_privacy_policy);
         agreeBtn = (Button) findViewById(R.id.useragreeBtn);
         agreeBtn.setOnClickListener(this);
 
-        textViewTermsOfService.setOnClickListener(this);
         textViewPrivacyPolicy.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.textview_terms_of_service){
-            startActivity(new Intent(TermsAndConditionsActivity.this, TermsPolicyActivity.class));
-        }else if(v.getId()==R.id.textview_privacy_policy){
+       if(v.getId()==R.id.textview_privacy_policy){
              startActivity(new Intent(TermsAndConditionsActivity.this, PrivacyActivity.class));
         }else if(v.getId()==R.id.useragreeBtn){
             startActivity(new Intent(TermsAndConditionsActivity.this,NavigationActivity.class));
