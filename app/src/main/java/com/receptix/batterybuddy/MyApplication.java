401649e1819d1e0cdaf3502467e175ab258b447d
kunalchawla <kunalchawla@zero1.io>
2017-05-31 13:37:49 +0530

    integrated clevertap tracking sdk.

--- app/src/main/java/com/receptix/batterybuddy/MyApplication.java
+++ app/src/main/java/com/receptix/batterybuddy/MyApplication.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.inmobi.sdk.InMobiSdk;
@@ -17,7 +19,7 @@ import static com.receptix.batterybuddy.helper.Constants.BannerPlacementIds.INMO
  * Created by futech on 23-May-17.
  */
 
-public class MyApplication extends MultiDexApplication {
+public class MyApplication extends com.clevertap.android.sdk.Application {
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -28,9 +30,19 @@ public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        // From CleverTap Documentation -
+        // If you have a custom Application class, call ActivityLifecycleCallback.register(this);
+        // before super.onCreate() in your class
+        ActivityLifecycleCallback.register(this);
+
         LogUtil.e("MyApplication", "onCreate()");
         // initialize InMobi SDK
         InMobiSdk.init(this, INMOBI_ACCOUNT_ID);
         InMobiSdk.setLogLevel(InMobiSdk.LogLevel.DEBUG);
+        if (BuildConfig.DEBUG) {
+            // During development, we set the SDK to debug mode,
+            // in order to log warnings or other important messages to Android logging system.
+            CleverTapAPI.setDebugLevel(1); // optional
+        }
     }
 }
